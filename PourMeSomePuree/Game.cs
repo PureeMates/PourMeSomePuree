25b03939b58e7f65f1f22101dd4fdba4f5b72a34
Graced <simone.bisato37@gmail.com>
2022-01-25 21:59:33 +0100

    Game
    Game

---
+++ PourMeSomePuree/Game.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aiv.Fast2D;
+using OpenTK;
+
+
+namespace PourMeSomePuree
+{
+
+    class Game
+    {
+        private static Window window;
+
+        public static Window Win { get { return window; } }
+
+
+        public static void Init()
+        {
+            window = new Window(1920, 1080, "PourMeSomePuree");
+           
+        }
+
+        public static void Play()
+        {
+            while (window.IsOpened)
+            {
+                //INPUT
+                
+
+                //UPDATE
+               
+
+                //DRAW
+                
+
+                window.Update();
+            }
+        }
+    }
+}
