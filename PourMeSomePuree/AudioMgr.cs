ce9a731871c9de3798a435770a60a6e0ac47d50c
ColoTech <colombo.emanuele96@gmail.com>
2022-02-05 14:10:18 +0100

    Development
    class Gfx and Audio

---
+++ PourMeSomePuree/AudioMgr.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aiv.Fast2D;
+using Aiv.Audio;
+using OpenTK;
+
+namespace PourMeSomePuree
+{
+    class AudioMgr
+    {
+        AudioSource bgSource = new AudioSource();
+        AudioClip bgMusic = new AudioClip("Assets/Audio/Horde_theme.ogg");
+
+        public void Input()
+        {
+            bgSource.Stream(bgMusic, win.DeltaTime);
+        }
+
+        public void Update()
+        {
+
+        }
+
+    }
+}
---
+++ PourMeSomePuree/GfxMgr.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aiv.Fast2D;
+using OpenTK;
+
+namespace PourMeSomePuree
+{
+    static class GfxMgr
+    {
+        private static Dictionary<string, Texture> textures;
+
+        static GfxMgr()
+        {
+            textures = new Dictionary<string, Texture>();
+        }
+
+        public static Texture AddTexture(string name, string path)
+        {
+            Texture t = new Texture(path);
+
+            if (t != null)
+            {
+                textures[name] = t;
+            }
+
+            return t;
+        }
+
+        public static Texture GetTexture(string name)
+        {
+            Texture t = null;
+
+            if (textures.ContainsKey(name))
+            {
+                t = textures[name];
+            }
+
+            return t;
+        }
+
+        public static void ClearAll()
+        {
+            textures.Clear();
+        }
+    }
+}
